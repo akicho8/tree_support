d080a17e6facfcd882039e2498fc564e01abea9d
akicho8 <akicho8@gmail.com>
2015-03-18 14:47:39 +0900

    to_json が動かない！？

--- examples/0220_to_json_bug.rb
+++ examples/0220_to_json_bug.rb
@@ -1,48 +1,31 @@
-# # -*- coding: utf-8 -*-
-# #
-# # TreeSupport::ArTreeModel を使った例
-# #
-# require "bundler/setup"
-# 
-# require "rails"
-# require "active_record"
-# require "tree_support"
-# require "byebug"
-# 
-# Class.new(Rails::Application){config.eager_load = false}.initialize! # Railtie を読ませるため
-# 
-# ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => ":memory:")
-# ActiveRecord::Migration.verbose = false
-# 
-# ActiveRecord::Schema.define do
-#   create_table :nodes do |t|
-#     t.belongs_to :parent
-#     t.string :name
-#   end
-# end
-# 
-# class Node < ActiveRecord::Base
-#   ar_tree_model scope: -> { order(:name).where.not(:name => "休憩") }
-# 
-#   def add(name, &block)
-#     tap do
-#       child = children.create!(:name => name)
-#       if block_given?
-#         child.instance_eval(&block)
-#       end
-#     end
-#   end
-# end
-# 
-# Node.create!(:name => "*root*").tap do |n|
-#   n.instance_eval do
-#     add "交戦" do
-#     end
-#   end
-# end
-# 
-# # p Node.first.as_json
-# r = Node.first
-# p r.to_a.first.to_json
-# # p ActiveRecord::Coders::JSON.dump(r)
+# -*- coding: utf-8 -*-
+#
+# ActiveRecord のレコードが Enumerable だと to_json が空になる現象
+# to_xml だと問題ない
+#
+require "bundler/setup"
 
+require "rails"
+require "active_record"
+require "tree_support"
+require "byebug"
+
+Class.new(Rails::Application){config.eager_load = false}.initialize! # Railtie を読ませるため
+
+ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => ":memory:")
+ActiveRecord::Migration.verbose = false
+
+ActiveRecord::Schema.define do
+  create_table :nodes do |t|
+    t.belongs_to :parent
+    t.string :name
+  end
+end
+
+class Node < ActiveRecord::Base
+  ar_tree_model
+end
+
+Node.create!(:name => "*root*")
+Node.first.to_json              # => "[]"
+Node.first.to_xml               # => "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<node>\n  <id type=\"integer\">1</id>\n  <parent-id type=\"integer\" nil=\"true\"/>\n  <name>*root*</name>\n</node>\n"
