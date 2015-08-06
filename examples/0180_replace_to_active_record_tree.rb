79f1a0877a0029e12b7bbac6922b405be8f4a1c3
akicho8 <akicho8@gmail.com>
2015-08-06 14:54:26 +0900

    TreeSupport::Node から AR への置き換えサンプル改善

--- examples/0180_replace_to_active_record_tree.rb
+++ examples/0180_replace_to_active_record_tree.rb
@@ -21,14 +21,84 @@ class Node < ActiveRecord::Base
   has_many :children, :class_name => name, :foreign_key => :parent_id
 end
 
-f = -> root, node {
-  sub = root.children.create!(:name => node.name)
-  node.children.each {|node| f.(sub, node) }
-}
-root = Node.create!(:name => TreeSupport.example.name)
-TreeSupport.example.children.each {|node| f.(root, node)}
-
+# 方法1. シンプルだけどユニークキー相当が必須になってしまう
+Node.destroy_all
+TreeSupport.example.each_node do |node|
+  obj = Node.find_or_initialize_by(:name => node.name) # 更新するために find_or にしている
+  if node.parent
+    obj.parent = Node.find_by(:name => node.parent.name) # ← ユニークキーがないとここで引くのが難しい
+  else
+    obj.parent = nil
+  end
+  obj.save!
+end
+root = Node.find_by(:name => TreeSupport.example.name)
 puts TreeSupport.tree(root)
+
+# 方法2. 子が作られるときは親がかならず存在することを利用して方法1を改良
+Node.destroy_all
+stock = {}
+TreeSupport.example.each_node do |node|
+  obj = Node.find_or_initialize_by(:name => node.name)
+  if node.parent
+    obj.parent = stock[node.parent]
+  else
+    obj.parent = nil
+  end
+  obj.save!
+  stock[node] = obj
+end
+root = Node.find_by(:name => TreeSupport.example.name)
+puts TreeSupport.tree(root)
+
+# 方法3. 再帰にするとユニークキーは不要。だけど create! が二箇所なのがちょっと。更新が難しい(？)
+Node.destroy_all
+def create_recursion(root, node)
+  sub = root.children.create!(:name => node.name)
+  node.children.each {|node| create_recursion(sub, node) }
+end
+root = Node.create!(:name => TreeSupport.example.name)
+TreeSupport.example.children.each {|node| create_recursion(root, node)}
+puts TreeSupport.tree(root)
+
+# >> *root*
+# >> ├─交戦
+# >> │   ├─攻撃
+# >> │   │   ├─剣を振る
+# >> │   │   ├─攻撃魔法
+# >> │   │   │   ├─召喚A
+# >> │   │   │   └─召喚B
+# >> │   │   └─縦で剣をはじく
+# >> │   └─防御
+# >> ├─撤退
+# >> │   ├─足止めする
+# >> │   │   ├─トラップをしかける
+# >> │   │   └─弓矢を放つ
+# >> │   └─逃走する
+# >> └─休憩
+# >>     ├─立ち止まる
+# >>     └─回復する
+# >>         ├─回復魔法
+# >>         └─回復薬を飲む
+# >> *root*
+# >> ├─交戦
+# >> │   ├─攻撃
+# >> │   │   ├─剣を振る
+# >> │   │   ├─攻撃魔法
+# >> │   │   │   ├─召喚A
+# >> │   │   │   └─召喚B
+# >> │   │   └─縦で剣をはじく
+# >> │   └─防御
+# >> ├─撤退
+# >> │   ├─足止めする
+# >> │   │   ├─トラップをしかける
+# >> │   │   └─弓矢を放つ
+# >> │   └─逃走する
+# >> └─休憩
+# >>     ├─立ち止まる
+# >>     └─回復する
+# >>         ├─回復魔法
+# >>         └─回復薬を飲む
 # >> *root*
 # >> ├─交戦
 # >> │   ├─攻撃
