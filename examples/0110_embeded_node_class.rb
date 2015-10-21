ea00409997368206b8a9ce63f1480a2fed7149be
akicho8 <akicho8@gmail.com>
2015-10-21 14:05:36 +0900

    Rename

--- examples/0110_embeded_node_class.rb
+++ examples/0110_embeded_node_class.rb
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# TreeSupport::Node は汎用的に使える
+# ちょっとした木構造を表現したいときに組み込みの TreeSupport::Node は汎用的に使える
 #
 require "bundler/setup"
 require "tree_support"
@@ -14,3 +14,14 @@ node.key                        # => :foo
 node = TreeSupport::Node.new(:a => 1, :b => 2)
 node[:a]                        # => 1
 node.to_h                       # => {:a=>1, :b=>2}
+
+tree = TreeSupport::Node.new(:root) do
+  add :a do
+    add :b do
+      add :c
+    end
+  end
+end
+
+tree.each_node.find{|e|e.key == :root}.key # => :root
+tree.each_node.find{|e|e.key == :b}.key    # => :b
